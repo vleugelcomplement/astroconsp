125c80faec0848526129db1a02ac719acb5bc354
Iliya Tikhonenko <iliya.t@mail.ru>
2017-06-03 14:29:11 +0300

    updated prepare.pl, added bits of my vim setup

--- tools/prepare.pl
+++ tools/prepare.pl
@@ -2,28 +2,36 @@
 use strict;
 use warnings;
 
-my ($fname) = @ARGV;
+## How about a generic template engine?
+# Or just use snips manager instead of this stuff?
+# Require considerations
+# Not today (2017-06-03)
+##
+
+my ($fname, $type) = @ARGV;
 my ($dirname) = ($fname =~ m/^(.*)\//);
 system("mkdir -p $dirname") if ( $dirname ); 
 open my $file, '>', $fname or die $!;
 
-my $com; #look of comment sign
-my $lang; #some default language constructions
-if ($fname =~ m/(?:\.c|\.cpp|\.cxx|\.cc|\.h)/) {
+my $com  = '#'; #comment sign
+my $lang = ''; #some default language constructions
+my $modeline = '';
+if ($fname =~ m/(?:\.c|\.cpp|\.cxx|\.cc|\.h)$/) {
     $com = '//';
-    
 }
-elsif ($fname =~ m/(?:\.sql|\.hs)/) {
+elsif ($fname =~ m/(?:\.sql|\.hs)$/) {
     $com = '--';
 }
-elsif ($fname =~ m/\.pl/) {
+elsif ($fname =~ m/\.pl$/) {
     $com = '#';
     $lang = "use strict;\nusewarnings;\n"
 }
-elsif ($fname =~ m/\.tex/) {
+elsif ($fname =~ m/(?:\.tex|\.ltx)$/) {
     $com = '%';
-    $lang =<< 'EOS'
-\documentclass[12pt]{../../../notes}
+    my $subfile =<< 'EOS';
+\documentclass[12pt,draft,timbord]{longnotes}
+\usepackage{tmath}
+\usepackage{cussymb}
 \usepackage{silence}
 \WarningFilter{latex}{Reference}
 \graphicspath{{../../img/}}
@@ -32,21 +40,32 @@ elsif ($fname =~ m/\.tex/) {
 
 \end{document}
 EOS
+    my $mainfile =<<'EOS'; 
+\documentclass[timbord]{longnotes}
+\usepackage{tmath}
+\usepackage{cussymb}
+\graphicspath{{img/}}
+
+\begin{document}
+
+\end{document}
+EOS
+    $lang = (defined($type) && $type eq "sub") ? $subfile : $mainfile; chomp $lang;
+    $modeline = $com." "."vim:tw=100 cc=100";
 }
-else {
-    $com = '#';
-}
+
 my $border = '-'x60;
 my $name = `git config user.name`; chomp $name;
 my $email = `git config user.email`; chomp $email;
 my $date = `LANG=C date`; chomp $date;
-print $file <<EOS
+print $file <<EOS;
 $com$border
 $com Description : 
 $com Author      : $name <$email>
 $com Created at  : $date
 $com$border
 $lang
+$modeline
 EOS
 
 
